e17d11e980b770ef841bb1bbce597c1c189dfd08
nuriarivasbotello <nuriarivasbotello@gmail.com>
2023-05-25 13:01:48 +0200

    update

--- src/form.php
+++ src/form.php
@@ -2,12 +2,12 @@
 //Campos del formulario
 $name = $_POST['name']; // Cada campo del formulario irá con $_POST['nombreDelCampo']
 $email = $_POST['email'];
-$message = $_POST['message'];
+$formMessage = $_POST['form-message'];
 //Atributos del correo    
 $from = "info@nuriarivas.com"; // el email desde donde se envía, en tu caso será la cuenta de correo que hayas creado.
 $to = "nuriarivasbotello@gmail.com"; // Cuenta de correo a la que se te envía
 $subject = "Checking PHP mail"; // Asunto
-$message = "El nombre del formulario es " . $name; // El mensaje del correo, funciona igual que los strings de javascript, sólo que el lugar de poner + para juntar texto con variables, se usa el punto 
+$message = "El nombre del formulario es " . $name . " Su email es " . $email . " El mensaje es " . $formMessage; // El mensaje del correo, funciona igual que los strings de javascript, sólo que el lugar de poner + para juntar texto con variables, se usa el punto 
 $headers = "From:" . $from;
 
 
